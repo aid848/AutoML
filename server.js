f4743310e6e6c035789d082c93d7d782bb29b6f4
Povel Batth <povel62@gmail.com>
2021-08-05 03:06:21 -0400

    Adding dotenv local config

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 let cors = require("cors");
 const port = process.env.PORT || 5000;
 const fileUpload = require("express-fileupload");
+const dotenv = require("dotenv");
 
 const uri =
   "***REMOVED***";
@@ -36,6 +37,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(express.methodOverride());
 // app.use(express.multipart());
+
+dotenv.config();
 
 // API calls
 app.get("/api/hello", (req, res) => {
